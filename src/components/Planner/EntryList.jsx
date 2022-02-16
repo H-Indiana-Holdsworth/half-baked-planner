88bffbde9d466ce905d309a70929c3025b4b2b1c
Hayden Indiana Holdsworth <IndianaHoldsworth@gmail.com>
2022-02-16 13:04:21 -0700

    Removes comment

--- src/components/Planner/EntryList.jsx
+++ src/components/Planner/EntryList.jsx
@@ -6,15 +6,6 @@ export default function Entries() {
   // TODO: Replace with the entries stored in PlannerContext
   const { entries } = useEntries();
 
-  // const entries = [
-  //   {
-  //     id: 0,
-  //     title: 'Replace me!',
-  //     content: '`content` should only be visible on the detail view',
-  //     date: new Date()
-  //   },
-  // ];
-
   return (
     <ul className={styles.list}>
       {entries.map(({ id, title, date }) => (
